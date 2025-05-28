9b64629307db771af343d0f9b24829b32da378cd
JoaoVictorFreitasBueno <joaovictorfreitas3887766@gmail.com>
2025-05-28 15:22:25 +0000

    Nivel Iniciante

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,63 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    char coluna[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    int tabuleiro[10][10] = {0};  // 0 = água, 1 = Submarino, 2 = Contratorpedeiro
+    int opcao;
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    printf("Bem Vindo ao jogo Batalha Naval!\n");
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    printf("Menu Inicial:\n");
+    printf("1. Jogar\n");
+    printf("2. Sair\n");
+    printf("Escolha uma opcao e pressione ENTER: ");
+    scanf("%d", &opcao);
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    switch (opcao) {
+        case 1:
+            printf("Iniciando o jogo...\n\n");
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+        //Posicionamento dos barco 1 (Horizontal)
+            tabuleiro[0][5] = 1; // A1
+            tabuleiro[0][6] = 1; // A2
+            tabuleiro[0][7] = 1; // A3
+
+        //posicionamento barco 2 (Vertical)
+            tabuleiro[3][3] = 2; // D4
+            tabuleiro[4][3] = 2; // E4
+            tabuleiro[5][3] = 2; // F4
+
+            
+            printf("  ");
+            for (int j = 0; j < 10; j++) {
+                printf(" %c", coluna[j]);
+            }
+            printf("\n");
+            
+            for (int i = 0; i < 10; i++) {
+                printf("%c ", linha[i]);
+                for (int j = 0; j < 10; j++) {
+                    if (tabuleiro[i][j] == 1)
+                        printf(" 3");  // Barco 1
+                    else if (tabuleiro[i][j] == 2)
+                        printf(" 3");  //Barco 2
+                    else
+                        printf(" 0"); // Água
+                }
+                printf("\n");
+            }
+
+            break;
+
+        case 2:
+            printf("Saindo do jogo. Obrigado por jogar!\n");
+            break;
+
+        default:
+            printf("Opcao invalida. Tente novamente.\n");
+            break;
+    }
 
     return 0;
 }
