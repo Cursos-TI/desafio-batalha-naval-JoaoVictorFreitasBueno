93bcfe35c8075fef7573055c6b6444e6271078a2
JoaoVictorFreitasBueno <joaovictorfreitas3887766@gmail.com>
2025-05-29 01:33:32 +0000

    Nivel Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+#define LINHAS 10
+#define COLUNAS 10
+// Definição das constantes para o número de linhas e colunas da matriz
+
 int main() {
     char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     char coluna[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-    int tabuleiro[10][10] = {0};  // 0 = água, 1 = Submarino, 2 = Contratorpedeiro
+    int tabuleiro[10][10] = {0}; // ~ = água, 1 = Barco 1, 2 = Barco 2, 3 = Barco 3, 4 = Barco 4
     int opcao;
 
     printf("Bem Vindo ao jogo Batalha Naval!\n");
@@ -18,7 +22,7 @@ int main() {
         case 1:
             printf("Iniciando o jogo...\n\n");
 
-        //Posicionamento dos barco 1 (Horizontal)
+        //Posicionamento do barco 1 (Horizontal)
             tabuleiro[0][5] = 1; // A5
             tabuleiro[0][6] = 1; // A6
             tabuleiro[0][7] = 1; // A7
@@ -28,22 +32,41 @@ int main() {
             tabuleiro[4][3] = 2; // E4
             tabuleiro[5][3] = 2; // F4
 
+            //Posicionamento do barco 3 (Diagonal)
+            tabuleiro[3][0] = 3; // A5
+            tabuleiro[2][1] = 3; // A6
+            tabuleiro[1][2] = 3; // A7
+
+            //Posicionamento do barco 4 (Diagonal)
+            tabuleiro[7][6] = 4; // A5
+            tabuleiro[8][7] = 4; // A6
+            tabuleiro[9][8] = 4; // A7
+
             
             printf("  ");
             for (int j = 0; j < 10; j++) {
-                printf(" %c", coluna[j]);
+                printf(" %c", coluna[j]); // Imprime as colunas
             }
             printf("\n");
             
             for (int i = 0; i < 10; i++) {
-                printf("%c ", linha[i]);
+                printf("%c ", linha[i]); // Imprime as linhas
+
                 for (int j = 0; j < 10; j++) {
-                    if (tabuleiro[i][j] == 1)
+                        if (tabuleiro[i][j] == 1)
                         printf(" 3");  // Barco 1
+
                     else if (tabuleiro[i][j] == 2)
-                        printf(" 3");  //Barco 2
+                        printf(" 3");   //Barco 2
+
+                    else if (tabuleiro[i][j] == 3)
+                        printf(" 3");  // Barco 3
+
+                    else if (tabuleiro[i][j] == 4)
+                        printf(" 3");  // Barco 4
+
                     else
-                        printf(" 0"); // Água
+                        printf(" ~"); // Achei mais interessante usar ~ para representar água
                 }
                 printf("\n");
             }
